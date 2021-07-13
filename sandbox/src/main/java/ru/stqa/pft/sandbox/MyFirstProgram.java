edfa460a1222f34d29e052b5afe4a84e68c30ae0
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2021-07-13 13:01:40 +0300

    Move functions to classes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,23 +7,15 @@ public class MyFirstProgram {
         hello("Nurshat");
 
         Square s = new Square(5.0);
-        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + area(s));
+        System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
         Rectangle r = new Rectangle(2.0, 5.0);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
 
     }
 
     public static void hello(String somebody){
         System.out.println("Hello, " + somebody + "!");
-    }
-
-    public static double area(Square s){
-        return s.l * s.l;
-    }
-
-    public static double area(Rectangle r){
-        return r.a * r.b;
     }
 
 }
