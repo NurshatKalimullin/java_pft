6e76989190cd5ea52c1475790f7d931e5201ad4c
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2021-08-29 17:13:30 +0300

    Created new module for Mantis tests

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+
+    protected static final ApplicationManager app
+            = new ApplicationManager (System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
