4365faf8b8129d08edc7521424aadacc88a6f5a2
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2021-09-02 00:15:50 +0300

    Add James Helper

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -15,7 +15,7 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
-    @BeforeMethod
+    //@BeforeMethod
     public void startMailServer(){
         app.mail().start();
     }
@@ -23,11 +23,14 @@ public class RegistrationTests extends TestBase {
     @Test
     public void testRegistration() throws MessagingException, IOException {
         long now = System.currentTimeMillis();
-        String email = String.format("user%s@localhost.localdomain", now);
+        String email = String.format("user%s@localhost", now);
         String user = String.format("user%s", now);
         String password = "password";
+        app.james().createUser(user, password);
+        app.james().drainEmail(user, password);
         app.registration().start(user, email);
-        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        List<MailMessage> mailMessages = app.james().waitForMail(user, password, 70000);
         String confirmarionLink = findConfirmarionLink(mailMessages, email);
         app.registration().finish(confirmarionLink, password);
         assertTrue(app.newSession().login(user,password));
@@ -39,7 +42,7 @@ public class RegistrationTests extends TestBase {
         return regex.getText(mailMessage.text);
     }
 
-    @AfterMethod(alwaysRun = true)
+    //@AfterMethod(alwaysRun = true)
     public void stopMailServer(){
         app.mail().stop();
     }
