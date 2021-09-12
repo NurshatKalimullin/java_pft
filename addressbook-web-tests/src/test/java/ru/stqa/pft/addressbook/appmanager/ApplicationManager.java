477217fbff7d654dfe60b81dae209872133fd682
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2021-09-12 20:37:24 +0300

    Add comment

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -53,7 +53,7 @@ public class ApplicationManager {
         } else {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
-            //capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
+            //capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10"))); //this line creates conflict in CI and must be commented
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
